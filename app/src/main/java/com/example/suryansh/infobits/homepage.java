7886dbb09bcf8ceb53dae209919080e35ba88ed1
sharmax <abhishake1204@hotmail.com>
2016-02-07 16:26:46 +0530

    Mipmap Images + Initial activity code of Communication Panel + AppIcon +  some changes here and there

--- app/src/main/java/com/example/suryansh/infobits/homepage.java
+++ app/src/main/java/com/example/suryansh/infobits/homepage.java
@@ -1,8 +1,7 @@
+package com.example.suryansh.infobits;
 /**
  * Created by Suryansh on 2/3/2016.
  */
-package com.example.suryansh.infobits;
-
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -13,7 +12,6 @@ public class homepage extends Login{
     ViewPager viewPager;
     Swipe_adapter adapter;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +19,6 @@ public class homepage extends Login{
         viewPager = (ViewPager)findViewById(R.id.view_pager);
         adapter = new Swipe_adapter(this);
         viewPager.setAdapter(adapter);
-
-
     }
 
 }
