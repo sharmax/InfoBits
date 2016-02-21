df4322b974333629bb1e6324a113e6faf5e2ac9e
Suryansh Rastogi <suryanshr123@gmail.com>
2016-02-20 17:14:50 -0800

    Latest Updates !

--- app/src/main/java/com/example/suryansh/infobits/homepage.java
+++ app/src/main/java/com/example/suryansh/infobits/homepage.java
@@ -1,4 +1,6 @@
 package com.example.suryansh.infobits;
+
+import android.support.v7.app.AppCompatActivity;
 /**
  * Created by Suryansh on 2/3/2016.
  */
@@ -6,19 +8,28 @@ package com.example.suryansh.infobits;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class homepage extends Login{
+public class homepage extends AppCompatActivity {
 
+    Toolbar toolbar;
     ViewPager viewPager;
     Swipe_adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homepage);
+        setContentView(R.layout.activity_homepage);
+
+        toolbar = (Toolbar)findViewById(R.id.nav_toolbar);
+        setSupportActionBar(toolbar);
+
         viewPager = (ViewPager)findViewById(R.id.view_pager);
         adapter = new Swipe_adapter(this);
         viewPager.setAdapter(adapter);
-    }
+ }
+
 
 }
