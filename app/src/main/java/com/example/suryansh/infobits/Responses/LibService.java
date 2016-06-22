e17081a84612791b5ab629848ad9b618993d8b08
Suryansh Rastogi <suryanshr123@gmail.com>
2016-06-22 14:07:38 +0530

    Library Services page updated.......

---
+++ app/src/main/java/com/example/suryansh/infobits/Responses/LibService.java
@@ -0,0 +1,13 @@
+package com.example.suryansh.infobits.Responses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LibService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lib_service);
+    }
+}
