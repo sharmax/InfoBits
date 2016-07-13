76a0734b212cc87de6184fe8228f1829bcce28fa
sharmax <abhishake1204@hotmail.com>
2016-07-14 03:08:48 +0530

    Ebooks Page and Some other Changes

--- app/src/main/java/com/example/suryansh/infobits/LibService.java
+++ app/src/main/java/com/example/suryansh/infobits/LibService.java
@@ -1,18 +1,10 @@
 package com.example.suryansh.infobits;
 
 import android.content.Intent;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 
 public class LibService extends homepage {
 
